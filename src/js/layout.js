0edfb61124add71021ddc5b22d263192c202164b
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2024-01-27 15:16:33 +0000

    Solucionando errores

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
-
-import { Home } from "./views/Home.jsx";
-
-import injectContext from "./store/appContext";
+import ScrollToTop from "./component/scrollToTop.js";
+import { Home } from "./views/Home.js";
 import { Footer } from "./component/Footer.jsx";
-import { Contacts } from "./views/Contacts.jsx";
 import { AddContact } from "./views/AddContact.jsx";
+import { EditContact } from "./views/EditContact.jsx";
+import injectContext from "./store/appContext.js";
 
 //create your first component
 const Layout = () => {
@@ -21,11 +19,10 @@ const Layout = () => {
         <ScrollToTop>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/contacts" element={<Contacts />} />
             <Route path="/addcontact" element={<AddContact />} />
+            <Route path="/editcontact" element={<EditContact />} />
             <Route path="*" element={<h1>Not found!</h1>} />
-          </Routes>
-          <Footer />
+          </Routes>          
         </ScrollToTop>
       </BrowserRouter>
     </div>
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,36 @@
-const getState = ({ setStore }) => {
+const getState = ({ getStore, getActions, setStore }) => {
 	return {
 	  store: {
-		contacts: [],
+		contactList: [
 		
+		],
 	  },
 	  actions: {
+		addContact: (contact) => {
+		  const store = getStore();
+		  store.contactList.push(contact);
+		  console.log(store.contactList)
 		},
+		delete: (i) => {
+		  const store = getStore();
+		  setStore({
+			...store,
+			contactList: store.contactList.filter((_, index) => {
+			  return index != i;
+			}),
+		  });
+		},
+		contact: () => {
+		  fetch("https://assets.breatheco.de/apis/fake/contact/agenda/JamesWList", {
+			  method: "GET", 
+			  headers: {"content-type": "application/json"},
+  
+		  })
+		  .then((response)=> response.json())
+		  .then((data)=> setStore({contactList:data}))
+		  
+		}
+	  },
 	};
   };
   
